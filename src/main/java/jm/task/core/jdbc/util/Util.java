e132522f496752038d3da6c7a40fc2cd66f9079c
renJava <rendg68@gmail.com>
2023-05-25 19:23:16 +0300

    7.2-й день изучения задания ПП2-114

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,23 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/rendb_pp114?useSSL=false";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
